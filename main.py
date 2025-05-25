a4fe82fd71d91496cb6a43efca1cb5989ad2e3ba
Josueborba23 <ra202512521@univassouras.edu.br>
2025-05-25 17:29:03 -0300

    caminhos

--- main.py
+++ main.py
@@ -39,6 +39,27 @@ else :
 print('Opção invalida. Tente novamente')
 escolha_caminho(personagem)
 
+def aliança_secreta(personagem):
+    print("\nVocê treina com guerreiros da resistência.")
+    print("Aprende táticas de combate e se prepara para enfrentar Malkor.")
+    time.sleep(2)
+    labirinto_sombrio(personagem)
 
-    
+def floresta_ilusoes(personagem):
+    print("\nVocê entra na Floresta das Ilusões. O caminho muda constantemente.")
+    print("Escolha como navegar:")
+    print("1 - Seguir os sussurros misteriosos")
+    print("2 - Confiar apenas nos seus instintos")
+    escolha = input("Digite 1 ou 2: ")
 
+  if escolha == "1":
+        print("\nOs sussurros revelam um caminho oculto e você sai da floresta ileso.")
+        time.sleep(2)
+        deserto_guardiao(personagem)
+    elif escolha == "2":
+        print("\nA floresta te confunde, e você perde tempo precioso.")
+        time.sleep(2)
+        deserto_guardiao(personagem)
+    else:
+        print("Opção inválida. Tente novamente.")
+        floresta_ilusoes(personagem)
