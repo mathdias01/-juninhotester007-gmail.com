df66736904c5ee1185a04f135ccc0f350cd0bebb
Marcella Lima <marcellalima990@gmail.com>
2025-05-25 16:22:18 -0300

    continuação introdução

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import time
 def introducao():
-    
+    print("\nBem-vindo ao Reino de Veridia.")
+    print("O necromante Malkor ameaça destruir tudo. Você é um dos poucos sobreviventes de sua vila.")
+    print("Para derrotá-lo, você precisa encontrar a arma lendária.")
+    time.sleep(3)
+    escolha_personagem()
