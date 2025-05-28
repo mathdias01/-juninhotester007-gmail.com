0254a0b3b967669470fda689e31f62b5ca7e0671
mathdias01 <dias.moutinho34@gmail.com>
2025-05-28 10:52:09 -0300

    correção final.py

--- main.py
+++ main.py
@@ -26,18 +26,18 @@ def escolha_personagem():
     
     print(f"\nVocê escolheu ser um {personagem}.")
     time.sleep(1)
-    escolha_caminho(personagem)
-print ('agora você deve decidir como buscar a arma lendária')
-print ('1 - A Aliança Secreta (Estudar magia proibida')
-print ('2 - A Força Bruta (Treinar com guerreiros experientes')
- escolha = input('Digite 1 ou 2: ')
- if escolha == '1':
-     aliança_secreta(personagem)
- elif escolha == '2':
-     forca_brita(personagem)
-else :
-print('Opção invalida. Tente novamente')
-escolha_caminho(personagem)
+def escolha_caminho(personagem):
+    print ('agora você deve decidir como buscar a arma lendária')
+    print ('1 - A Aliança Secreta (Estudar magia proibida')
+    print ('2 - A Força Bruta (Treinar com guerreiros experientes')
+    escolha = input('Digite 1 ou 2: ')
+    if escolha == '1':
+        aliança_secreta(personagem)
+    elif escolha == '2':
+     forca_bruta(personagem)
+    else :
+        print('Opção invalida. Tente novamente')
+        escolha_caminho(personagem)
 
 def aliança_secreta(personagem):
     print("\nVocê encontra uma sociedade secreta de magos.")
@@ -45,11 +45,13 @@ def aliança_secreta(personagem):
     time.sleep(2)
     floresta_ilusoes(personagem)
 
- def forca_bruta(personagem):
+
+def forca_bruta(personagem):
     print("\nVocê treina com guerreiros da resistência.")
     print("Aprende táticas de combate e se prepara para enfrentar Malkor.")
     time.sleep(2)
     labirinto_sombrio(personagem)
+
 
 def floresta_ilusoes(personagem):
     print("\nVocê entra na Floresta das Ilusões. O caminho muda constantemente.")
@@ -58,7 +60,7 @@ def floresta_ilusoes(personagem):
     print("2 - Confiar apenas nos seus instintos")
     escolha = input("Digite 1 ou 2: ")
 
-  if escolha == "1":
+    if escolha == "1":
         print("\nOs sussurros revelam um caminho oculto e você sai da floresta ileso.")
         time.sleep(2)
         deserto_guardiao(personagem)
@@ -89,7 +91,7 @@ def labirinto_sombrio(personagem):
         print("Opção inválida. Tente novamente.")
         labirinto_sombrio(personagem)
 
-def deserto_guardiao(personagem)
+def deserto_guardiao(personagem):
     print("\nVocê chega ao templo onde a arma lendária está escondida.")
     print("Mas um guardião místico protege a entrada.")
     print("Escolha seu desafio: ")
